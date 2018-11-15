3e67c7390562e9d984460ffdcfd0fc50a4b909d7
FeelsMood <andrzej@MacBook-Air-Andrew.local>
2018-11-15 22:00:23 +0100

    .

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Admin Panel</title>
+    <link rel="stylesheet" href="css/style.css">
     <?php
         require_once 'connection.php';
 
@@ -20,6 +21,26 @@
     ?>
 </head>
 <body>
+    <form method="POST" class="login">
+        <input name='login' placeholder="Login">
+		<input name='password' placeholder="Password" type='password'>
+		<input type='submit' value='Login' class="submit">
+    </form>
+    <?php
+        if( !empty($_REQUEST['password']) and !empty($_REQUEST['login']) ) {
+            $login = $_REQUEST['login'];
+            $password = $_REQUEST['password'];
+        }
 
+        $query = 'SELECT*FROM admins WHERE username="'.$login.'" AND password="'.$password.'"';
+        $result = mysqli_query($link, $query);
+        $users = mysqli_fetch_assoc($result);
+
+        if (!empty($user)) {
+            echo "FUCK YYEA!";
+        } else {
+            echo "YOU FUCKING LOOSER!";
+        }
+    ?>
 </body>
 </html>
