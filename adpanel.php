000e80fe02eaf32546c07c53cc054b13bcbdee50
FeelsMood <andrzej@MacBook-Air-Andrew.local>
2018-11-18 14:21:06 +0100

    nw co jest, nie działa dodawanie uzytkownika

--- adpanel.php
+++ adpanel.php
@@ -23,23 +23,61 @@
 <body>
     Admin Panel <br /><br />
 
-    <?php
+    <!-- users  -->
+    <div class="users">
+        <h1>Users</h1>
+        <?php
 
-        $qid = mysqli_query($link, 'SELECT * FROM admins');
-        while($row = mysqli_fetch_assoc($qid)) {
-                    print($row['username'] . "<br>");
-                }
-    ?>
+            $qid = mysqli_query($link, 'SELECT * FROM users');
+            while($row = mysqli_fetch_assoc($qid)) {
+                        print($row['username'])." - ";
+                        print($row['password']);
+                    }
+        ?>
+    </div>
 
+     <!-- add user  -->
+     <div class="adduser">
+        <h1>Add User</h1>
+        <form method="post" name="form">
+            <p><input type="text" name="uname" required placeholder="Username"></p>
+            <p><input type="text" name="fname" required placeholder="First Name"></p>
+            <p><input type="text" name="lname" required placeholder="Last Name"></p>
+            <p><input type="email" name="email" required placeholder="Email"></p>
+            <p><input type="password" name="pass" required placeholder="Password"></p>
+            <p><input name="add" type="submit" value="add"></p>
+        </form>
+     </div>
+
+    <!-- wylogować się  -->
     <form method="post" name="form">
-        <p><input name="submit" type="submit" value="Wylogować się"></p>
+        <p><input name="out" type="submit" value="Wylogować się"></p>
     </form>
 
     <?php
-        if (isset($_POST['submit'])) {
+        if (isset($_POST['out'])) {
             unset($_SESSION['login']);
             unset($_SESSION['password']);
             header('Location: index.php');
+        }
+
+        if (isset($_POST['add'])) {
+            $host = 'localhost';
+            $database = 'Admin Panel';
+            $user = 'Admin';
+            $password = '123';
+
+            $link = mysqli_connect($host,$user,$password,$database) or die("Error " . mysqli_error($link));
+            $uname = $_POST['uname'];
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $email = $_POST['email'];
+            $pass = $_POST['pass'];
+            $query = "INSERT INTO 'users'(`user_id`, `username`, `password`, `first_name`, `last_name`, `email`) VALUES (NULL '$uname', '$pass','$fname', '$lname', '$email')";
+            $result = mysqli_query($link, $query);
+            if($result) {
+                echo 'Done!';
+            }
         }
     ?>
 </body>
